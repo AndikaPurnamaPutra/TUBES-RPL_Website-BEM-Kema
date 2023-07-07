54f2a27dd5717fcac56b62264303a9469f00f05d
AndikaPurnamaPutra <andikapratamaputra849@gmail.com>
2023-07-08 02:10:59 +0700

    progress Home Page

---
+++ assets/js/script-sign.js
@@ -0,0 +1,18 @@
+// Page Login & Register
+
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+
+    // toggle the icon
+    this.classList.toggle("bi-eye");
+});
+
+const form = document.querySelector(form);
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,10 @@
-const togglePassword = document.querySelector("#togglePassword");
-const password = document.querySelector("#password");
-
-togglePassword.addEventListener("click", function () {
-    // toggle the type attribute
-    const type = password.getAttribute("type") === "password" ? "text" : "password";
-    password.setAttribute("type", type);
-
-    // toggle the icon
-    this.classList.toggle("bi-eye");
-});
-
-const form = document.querySelector(form);
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
+// Navbar
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOMContentLoaded event triggered.");
+    navbarToggle = document.querySelector(".navbar-toggle");
+    navbarToggle.addEventListener("click", function () {
+        console.log("Navbar toggle clicked.");
+        navbarMenu = document.querySelector(".navbar-collapse");
+        navbarMenu.classList.toggle("active");
+    });
 });
