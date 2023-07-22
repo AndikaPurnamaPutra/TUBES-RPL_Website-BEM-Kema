d0c3052e3fe4aea81c635fe66d6c51ce6fc47f0b
AndikaPurnamaPutra <andikapratamaputra849@gmail.com>
2023-07-22 16:22:26 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
-// Navbar
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOMContentLoaded event triggered.");
-    navbarToggle = document.querySelector(".navbar-toggle");
-    navbarToggle.addEventListener("click", function () {
-        console.log("Navbar toggle clicked.");
-        navbarMenu = document.querySelector(".navbar-collapse");
-        navbarMenu.classList.toggle("active");
-    });
+const navbarToggler = document.querySelector(".navbar-toggle");
+
+navbarToggler.addEventListener("click", function () {
+    console.log("Navbar toggle clicked.");
+    navbarMenu = document.querySelector(".navbar-collapse");
+    navbarMenu.classList.toggle("active");
 });
+// Navbar
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     console.log("DOMContentLoaded event triggered.");
+//     navbarToggle = document.querySelector(".navbar-toggle");
+//     navbarToggle.addEventListener("click", function () {
+//         console.log("Navbar toggle clicked.");
+//         navbarMenu = document.querySelector(".navbar-collapse");
+//         navbarMenu.classList.toggle("active");
+//     });
+// });
